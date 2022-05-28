6574c442a14eee83b68f98e84e76ff597906601d
natsumi <aka.723n@gmail.com>
2022-05-28 18:03:16 +0900

    feat: add icon ボタン

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,24 @@
+import {AiOutlineCheck} from 'react-icons/ai'
+import {AiOutlineDelete} from 'react-icons/ai'
 
 export const TodoList = () => {
   return(
     <div className="todoList">
       <div className="todos">
-            <div className="todoText">
-              <span>プログラミング</span>
-            </div>
-            <div className="icons">
-              <button >
-              </button>
-              <button >
-              </button>
-            </div>
+        <div className="todo">
+          <div className="todoText">
+            <span>プログラミング</span>
           </div>
+          <div className="icons">
+            <button className="icons">
+              <AiOutlineCheck className="fa-check" size="2.5rem"/>
+            </button>
+            <button className="icons" >
+              <AiOutlineDelete className="fa-trash" size="2.5rem"/>
+            </button>
+          </div>
+        </div>
+      </div>
       </div>
   );
 }
