fb41cc167f8848237cc45ad0020f424b028e0411
natsumi <aka.723n@gmail.com>
2022-05-30 16:35:46 +0900

    feat: タスクリストに追加する

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import {Title} from './components/Title'
 import {InputForm} from './components/InputForm'
 import './App.css';
 import {TodoList} from "./components/TodoList";
+import {useState} from "react";
 
 
 
 function App() {
+
+  const [taskList, setTaskList] = useState([]);
+
   return (
     <div className="App">
       <Title />
-      <InputForm />
+      <InputForm taskList={taskList} setTaskList={setTaskList}/>
       <TodoList />
     </div>
   );
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,18 +1,31 @@
 import { FaGithub } from 'react-icons/fa';
 import { MdAddCircle } from 'react-icons/md';
+import {useState} from "react";
 
 
-export const InputForm = () => {
+export const InputForm = ({taskList, setTaskList}) => {
+
+  const [inputText, setInputText] = useState("");
 
   const handleSubmit = (e) => {
-    console.log("a");
     e.preventDefault();
+  //  タスクを追加する
+    setTaskList([
+      ...taskList,
+      {
+        text: inputText
+      }
+    ]);
   }
 
+  const handlrChange = (e) => {
+    setInputText(e.target.value);
+  }
+  
   return(
     <div className="inputForm">
       <form onSubmit={handleSubmit}>
-        <input className="" type="text"/>
+        <input className="" type="text" onChange={handlrChange}/>
         <button>
           <MdAddCircle size="2rem"/>
         </button>
